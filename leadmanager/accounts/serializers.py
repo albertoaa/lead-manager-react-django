b01310615f609fff317ec7f9f4d6ba54076462b5
Alberto Aragon <ronin8503@gmail.com>
2019-10-30 17:59:43 -0500

    loging in and getting an user

--- leadmanager/accounts/serializers.py
+++ leadmanager/accounts/serializers.py
@@ -2,16 +2,15 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
+
 # User Serializer
-
-
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = ('id', 'username', 'email')
+
+
 # Register Serializer
-
-
 class RegisterSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
@@ -23,4 +22,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         validated_data['username'], validated_data['email'], validated_data['password'])
     return user
 
+
 # Login Serializer
+class LoginSerializer(serializers.Serializer):
+  username = serializers.CharField()
+  password = serializers.CharField()
+
+  def validate(self, data):
+    user = authenticate(**data)
+    if user and user.is_active:
+      return user
+    raise serializers.ValidationError("Incorrect Credentials")
