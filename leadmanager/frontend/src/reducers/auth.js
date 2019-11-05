5df1d17475dd358a09702cbcfb30951dafd385b9
Alberto Aragon <ronin8503@gmail.com>
2019-11-04 21:39:10 -0500

    login user and redirecting to dashboard

--- leadmanager/frontend/src/reducers/auth.js
+++ leadmanager/frontend/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { USER_LOADED, USER_LOADING, AUTH_ERROR } from '../actions/types';
+import { USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -21,7 +21,8 @@ export default function(state = initialState, action) {
         isLoading: false,
         user: action.payload
       };
-    case AUTH_ERROR: {
+    case AUTH_ERROR:
+    case LOGIN_FAIL: {
       localStorage.removeItem('token');
       return {
         ...state,
@@ -29,6 +30,15 @@ export default function(state = initialState, action) {
         user: null,
         isLoading: false,
         isAuthenticated: false
+      };
+    }
+    case LOGIN_SUCCESS: {
+      localStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false
       };
     }
     default:
